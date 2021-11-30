e9a1e9179381b1ffcda2f5f1e83c7c5604cf1c54
rayanadir <rayan.dahmena@gmail.com>
2021-11-30 19:31:37 +0100

    préparation logique des filtres

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 import * as recipes from '../recipes.js';
 
-//console.log(recipes.recipes);
-
 var recipesArray = recipes.recipes;
 let recipesList = document.getElementById('recipes');
-
+let ingredientsFilter = document.getElementById('ingredients');
+let appareilsFilter = document.getElementById('appareils');
+let ustensilesFilter = document.getElementById('ustensiles');
+let chevron1 = document.getElementById('chevron1');
+let chevron2 = document.getElementById('chevron2');
+let chevron3 = document.getElementById('chevron3');
 
 function loadRecipes(array) {
     recipesList.innerHTML = ``;
-    for (var i = 0; i < array.length; i++) {
-        array[i].ingredients.map((a) => {
-            if (a.unit == undefined) {
-                a.unit = "";
-            }
-            if (a.quantity == undefined) {
-                a.quantity = "";
-            }
-        })
-        var template = `
+    //console.log(array.length)
+    if (array.length > 0) {
+        recipesList.style.display = "grid";
+        for (var i = 0; i < array.length; i++) {
+            array[i].ingredients.map((a) => {
+                if (a.unit == undefined) {
+                    a.unit = "";
+                }
+                if (a.quantity == undefined) {
+                    a.quantity = "";
+                }
+            })
+            var template = `
     <article class="recipes__recipe">
     <img src="/images/img.png" alt="image" class="recipes__image">
     <div class="recipes__details">
@@ -53,7 +59,16 @@ function loadRecipes(array) {
 </article>
     `
         recipesList.innerHTML += template;
+        }
+    }else{
+        recipesList.style.display="block";
+        recipesList.innerHTML=`
+        <p class="recipes__no_result">
+            Aucune rectette ne correspond à votre critère ... vous pouvez chercher "tarte aux pommes","poisson", etc ...
+        </p>
+        `
     }
+    
 }
 
 loadRecipes(recipesArray);
@@ -68,9 +83,34 @@ document.getElementById('search').addEventListener('input', (e) => {
                 resultRecipes.push(recipe);
             }
         })
-        loadRecipes(resultRecipes);
-        //console.log(resultRecipes);
-    }else{
-        resultRecipes=recipesArray;
+    } else {
+        resultRecipes = recipesArray;
     }
+    //console.log(resultRecipes);
+    loadRecipes(resultRecipes);
+})
+
+function getAllIngredients(array){
+    var ingredientsArray=[];
+    array.forEach((recipe)=>{
+        recipe.ingredients.forEach((ingredient)=>{
+            if(ingredientsArray.indexOf(ingredient.ingredient) == -1){
+                ingredientsArray.push(ingredient.ingredient)
+            }
+        })
+    });
+    //console.log(ingredientsArray);
+    return ingredientsArray;
+}
+
+getAllIngredients(recipesArray);
+
+ingredientsFilter.addEventListener('click', (e)=>{
+    ingredientsFilter.style.width="30rem";
+    chevron1.style.transform="rotate(180deg)";
+})
+
+chevron1.addEventListener('click', (e)=>{
+    ingredientsFilter.style.width="auto";
+    chevron1.style.transform="rotate(0deg)";
 })
